a4cbfcd8f1650e08139e7cec2bae4f7d71b76b35
ム乇 尺 1 乇 <121339626+ger1e@users.noreply.github.com>
2026-08-22 11:35:51 +0200

    feat: add custom API catch-all 404

---
+++ api/[...path].js
@@ -0,0 +1,5 @@
+import { renderHttpError, writeVercelResponse } from '../src/app.js';
+
+export default async function handler(req, res) {
+  writeVercelResponse(res, renderHttpError(req, 404, 'not_found'));
+}
