31d0f255e3bc1f8021ce6a38207c41fe6d73397a
ム乇 尺 1 乇 <121339626+ger1e@users.noreply.github.com>
2026-09-01 14:36:48 +0200

    feat: expose bounded mission core API

---
+++ src/core/mission/index.js
@@ -0,0 +1,5 @@
+export { normalizeClientProfile } from './client-profile.js';
+export { assessClientRelevance } from './relevance.js';
+export { validateMissionKql } from './kql-validator.js';
+export { buildHuntPackage } from './hunt-package.js';
+export { analyzeMissionResults } from './result-analysis.js';
